c53bb3fbf881c6195d9dee84e5eb9840a68b0c46
Lucas Fischer <fischer-lucas@hotmail.com>
2022-12-01 14:51:21 +0000

    update de aulas

---
+++ includes_repeat/scripts/script.js
@@ -0,0 +1,15 @@
+//Includes em array.
+
+let lista = ['ovo', 'café', 'arroz', 'feijão', 'macarrão'];
+
+console.log(lista.includes('ovo'));
+
+//Includes em string.
+
+let nome = 'Fischer';
+
+console.log(nome.includes('i'));
+
+// Repeat.
+
+console.log(nome.repeat(20));
---
+++ objects/scripts/script.js
@@ -0,0 +1,15 @@
+// objeto.
+let pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Fischer',
+    idade: 26
+};
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
+
+// array.
+let ingredientes = ['ovo', 'café', 'arroz', 'feijão', 'macarrão'];
+console.log(Object.keys(ingredientes));
+console.log(Object.values(ingredientes));
+console.log(Object.entries(ingredientes));
