9a3a2acaf6242e92ae51dac4af298da586678ebe
Lucas Fischer <fischer-lucas@hotmail.com>
2022-12-01 12:36:35 +0000

    update

---
+++ descontrucao_arrays/scripts/script.js
@@ -0,0 +1,5 @@
+let array =['Lucas Fischer', 'Lucas', 'Fischer', '@lucasfischer'];
+
+let [nomeCompleto, nome, sobrenome, instagram] = array;
+
+console.log(nomeCompleto, nome, sobrenome, instagram);
